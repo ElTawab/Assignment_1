d4f904630e103571e170f1b8979aaae8d4a1653a
Mo7md Yasser <mohamedeltawab712@gmail.com>
2021-07-10 15:28:02 +0200

    Student 1 structure

---
+++ Students_structure.h
@@ -0,0 +1,14 @@
+#ifndef _Students_structure_H_
+#define _Students_structure_H_
+
+typedef struct
+{
+	char name[28];
+	int ID;
+	 int score;              //score of last year of the student
+    int birthdate[3];        //element #0 =day  element #2=month    element #3=year
+    
+   
+    
+}student;
+#endif
